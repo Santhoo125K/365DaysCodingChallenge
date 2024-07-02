676625bc0a7251f6a2d3ef4a362ac12be5dc9cb8
Santosh N <147081083+Santhoo125K@users.noreply.github.com>
2024-07-02 22:35:26 +0530

    Time: 4 ms (39.02%), Space: 45.9 MB (53.44%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,19 +1,42 @@
+// class Solution {
+//     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+// //         int mergeArray[] = new int[nums1.length + nums2.length];
+        
+// //         System.arraycopy(nums1, 0, mergeArray, 0, nums1.length);
+// //         System.arraycopy(nums2, 0, mergeArray, nums1.length, nums2.length);
+        
+// //         Arrays.sort(mergeArray);
+        
+// //         int len = mergeArray.length;
+        
+        
+// //         if(mergeArray.length % 2 == 0){
+// //             return ((mergeArray[len/2 - 1]) + (mergeArray[len/2]))/2.0;
+// //         }else{
+// //             return mergeArray[len/2];
+// //         } 
+        
+        
+//     }
+// }
+
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int mergeArray[] = new int[nums1.length + nums2.length];
+        int arr[] = new int[nums1.length + nums2.length];
+        for(int i=0,j=0;i<arr.length;i++){
+            if(i>=nums1.length)
+                arr[i]=nums2[j++];
+            else
+                arr[i]=nums1[i];
+        }
+        Arrays.sort(arr);
+        double ans = 0.0;
         
-        System.arraycopy(nums1, 0, mergeArray, 0, nums1.length);
-        System.arraycopy(nums2, 0, mergeArray, nums1.length, nums2.length);
+        if(arr.length % 2 == 0)
+            ans = (double) (arr[arr.length/2 -1] + arr[arr.length/2]) / 2;
+        else
+            ans = arr[arr.length/2];
         
-        Arrays.sort(mergeArray);
-        
-        int len = mergeArray.length;
-        
-        
-        if(mergeArray.length % 2 == 0){
-            return ((mergeArray[len/2 - 1]) + (mergeArray[len/2]))/2.0;
-        }else{
-            return mergeArray[len/2];
-        } 
+        return ans;
     }
 }
