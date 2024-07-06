c1855792ca7ba6584e53492d23b6d3372ba8423f
Santosh N <147081083+Santhoo125K@users.noreply.github.com>
2024-07-06 21:17:19 +0530

    Time: 1 ms (99.00%), Space: 41.4 MB (54.56%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,27 +1,48 @@
+// class Solution {
+//     public boolean isValid(String s) {
+//         Stack<Character> st = new Stack<>();
+        
+//         Map<Character, Character> mp = new HashMap<>();
+//         mp.put('(',')');
+//         mp.put('{','}');
+//         mp.put('[',']');
+        
+//         for(char c : s.toCharArray()){
+//             if(mp.containsKey(c)){
+//                 st.push(c);
+//             }else if(mp.containsValue(c)){
+//                 if(st.isEmpty()){
+//                     return false;
+//                 }
+                
+//                 char top = st.pop();
+                
+//                 if(mp.get(top)!=c){
+//                     return false;
+//                 }
+//             }
+//         }
+//         return st.isEmpty();
+//     }
+// }
+
+
+
+
 class Solution {
     public boolean isValid(String s) {
         Stack<Character> st = new Stack<>();
         
-        Map<Character, Character> mp = new HashMap<>();
-        mp.put('(',')');
-        mp.put('{','}');
-        mp.put('[',']');
-        
-        for(char c : s.toCharArray()){
-            if(mp.containsKey(c)){
+        for(char c: s.toCharArray()){
+            if((c == ')' && !st.isEmpty() && st.peek() == '(') || (c == '}' && !st.isEmpty() && st.peek() == '{') || (c == ']' && !st.isEmpty() && st.peek() == '[')){
+                st.pop();
+            }
+            else{
                 st.push(c);
-            }else if(mp.containsValue(c)){
-                if(st.isEmpty()){
-                    return false;
-                }
-                
-                char top = st.pop();
-                
-                if(mp.get(top)!=c){
-                    return false;
-                }
             }
         }
+        
         return st.isEmpty();
+ 
     }
 }
