9d6d91ad82a5db69612969e16c01b1bbc6a87440
Santosh N <147081083+Santhoo125K@users.noreply.github.com>
2024-07-13 22:29:32 +0530

    Time: 7 ms (99.37%), Space: 43.3 MB (14.21%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.java
+++ 0016-3sum-closest/0016-3sum-closest.java
@@ -1,28 +1,42 @@
 class Solution {
     public int threeSumClosest(int[] nums, int target) {
+        int len = nums.length; 
+        if(len == 3) {return nums[0] + nums[1] + nums[2];}
         Arrays.sort(nums);
-        int closestSum = nums[0] + nums[1] + nums[2]; // Initialize closest sum with the sum of the first three elements
+        int ans = nums[0] + nums[1] + nums[2];
+        if(ans >= target) {return ans;}
+        int max = nums[len - 1] + nums[len - 2] + nums[len - 3];
+        if(max <= target) {return max;}
+        int last = nums[0];
+        int left, right, sum, num;
+        int dist = Math.abs(ans - target);
 
-        for (int i = 0; i < nums.length - 2; i++) {
-            int j = i + 1;
-            int k = nums.length - 1;
-
-            while (j < k) {
-                int sum = nums[i] + nums[j] + nums[k];
-
-                if (Math.abs(target - sum) < Math.abs(target - closestSum)) {
-                    closestSum = sum; // Update closest sum if the current sum is closer to the target
+        for(int i = 0; i < len - 2; i++){
+            if (i == last && nums[i] == last)
+                continue;
+            num = nums[i];
+            last = num;
+            left = i + 1;
+            right = len - 1;
+            while(left < right){
+                sum = num + nums[left] + nums[right];
+                if (sum == target) return sum;
+                if(Math.abs(sum - target) < dist){
+                    ans = sum;
+                    dist = Math.abs(ans - target);
                 }
-
-                if (sum < target) {
-                    j++; // Increment j to increase the sum
-                } else {
-                    k--; // Decrement k to decrease the sum
+                if(sum < target){ 
+                    while(left < right && nums[left] == nums[left + 1])
+                        left++;
+                    left++;
                 }
+                else{
+                     while(left < right && nums[right] == nums[right - 1])
+                        right--;
+                    right--;
+                }   
             }
         }
-
-        return closestSum;
-        
+        return ans;
     }
 }
