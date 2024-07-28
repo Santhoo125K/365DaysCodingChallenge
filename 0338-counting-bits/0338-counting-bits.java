96a222c865bf1cdbcf7bb573e2353f21b36f68ae
Santosh N <147081083+Santhoo125K@users.noreply.github.com>
2024-07-28 21:16:21 +0530

    Time: 0 ms (100.00%), Space: 46.4 MB (75.93%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -1,14 +1,29 @@
 class Solution {
-    public int[] countBits(int num) {
-        int result[] = new int[num + 1];
-        int offset = 1;
-        for (int index = 1; index < num + 1; ++index){
-            if (offset * 2 == index){
-                offset *= 2;
-            }
-        result[index] = result[index - offset] + 1;
+
+    public int[] countBits(int n) {
+        int[] arr = new int[n + 1];
+        countBits(arr, 1, 1);
+        return arr; 
+    }
+
+    public void countBits(int[] arr, int n, int ones) {
+        if (n >= arr.length) {
+            return;
         }
-        return result;
+        arr[n] = ones;
+        // shift left means * 2, ones will still be the same
+        countBits(arr, n << 1, ones);
+        // * 2 + 1 means number of ones will increase by 1
+        countBits(arr, (n << 1) + 1, ones + 1);
+    }
+
+    public int[] countBitsIter(int n) {
+        int[] arr = new int[n + 1];
+        for (int i = 1; i < arr.length; i++) {
+            // number of 1s at i is the same as number of 1s at i/2 because
+            // last bit is 0
+            arr[i] = i % 2 == 0 ? arr[i / 2] : 1 + arr[i / 2];
+        }
+        return arr;
     }
 }
-
