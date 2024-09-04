1d2ab7336b8d98f32e41f59f52279493c0dd2f00
Santosh N <147081083+Santhoo125K@users.noreply.github.com>
2024-09-04 22:34:57 +0530

    Time: 36 ms (42.79%), Space: 48.5 MB (94.32%) - LeetHub

--- 0874-walking-robot-simulation/0874-walking-robot-simulation.java
+++ 0874-walking-robot-simulation/0874-walking-robot-simulation.java
@@ -4,34 +4,30 @@ import java.util.Set;
 class Solution {
 
     public int robotSim(int[] commands, int[][] obstacles) {
-        // Define directions: North (0, 1), East (1, 0), South (0, -1), West (-1, 0)
-        int[][] directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-        int dir = 0; // Start facing north
 
-        // Set of obstacle positions
+        int[][] directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        int dir = 0; 
+
         Set<String> obstacleSet = new HashSet<>();
         for (int[] obstacle : obstacles) {
             obstacleSet.add(obstacle[0] + "," + obstacle[1]);
         }
 
-        int x = 0, y = 0; // Start at (0, 0)
+        int x = 0, y = 0; 
         int maxDistanceSquared = 0;
 
         for (int command : commands) {
             if (command == -2) {
-                // Turn left 90 degrees
                 dir = (dir + 3) % 4;
             } else if (command == -1) {
-                // Turn right 90 degrees
                 dir = (dir + 1) % 4;
             } else {
-                // Move forward `command` steps
                 for (int step = 0; step < command; step++) {
                     int nextX = x + directions[dir][0];
                     int nextY = y + directions[dir][1];
 
                     if (obstacleSet.contains(nextX + "," + nextY)) {
-                        break; // Hit an obstacle, stop moving
+                        break; 
                     } else {
                         x = nextX;
                         y = nextY;
