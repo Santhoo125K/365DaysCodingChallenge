a3ed7bf575d52cf7a2d758f0ca87f15e1aeb1b7e
Santosh N <147081083+Santhoo125K@users.noreply.github.com>
2024-10-06 17:22:15 +0530

    Time: 1 ms (96.43%), Space: 42.1 MB (8.33%) - LeetHub

---
+++ 1813-sentence-similarity-iii/1813-sentence-similarity-iii.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean areSentencesSimilar(String sentence1, String sentence2) {
+        // Split both sentences into words
+        String[] words1 = sentence1.split(" ");
+        String[] words2 = sentence2.split(" ");
+        
+        int n1 = words1.length, n2 = words2.length;
+        int i = 0, j = 0;
+        
+        // Compare prefix words (from the beginning)
+        while (i < n1 && i < n2 && words1[i].equals(words2[i])) {
+            i++;
+        }
+        
+        // Compare suffix words (from the end)
+        while (j < n1 - i && j < n2 - i && words1[n1 - 1 - j].equals(words2[n2 - 1 - j])) {
+            j++;
+        }
+        
+        // If the sum of matched prefix and suffix words is at least the length of the shorter sentence
+        return i + j >= Math.min(n1, n2);
+    }
+}
