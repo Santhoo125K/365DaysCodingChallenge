e1131eec5b294e7aba33cc2c86b41c09acd35999
Santosh N <147081083+Santhoo125K@users.noreply.github.com>
2025-01-01 21:36:51 +0530

    Time: 1 ms (97.36%), Space: 41.9 MB (14.87%) - LeetHub

---
+++ 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maxScore(String s) {
+    
+        int ones=0, zeros = 0, maxScore = 0;
+
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i) == '1')
+                ones++;
+        }
+
+        for(int i=0 ;i<s.length()-1;i++){
+            if(s.charAt(i) == '0'){
+                zeros++;
+            }
+            else{
+                ones--;
+            }
+            maxScore = Math.max(maxScore , zeros+ones);
+        }
+        return maxScore;
+    }
+}
